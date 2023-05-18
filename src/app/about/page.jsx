303b618639947385ebac6b8b9d9bd78724a492e4
Valeria <lerik13@gmail.com>
2023-05-18 11:38:04 -0600

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const metadata = {
+	title: 'About Next.js 13',
+}
+
+const AboutPage = () => {
+	return <div>
+		<h1>About project</h1>
+		<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam nemo esse, voluptas consequatur quos repellendus consequuntur incidunt! Rem error modi autem eligendi omnis. Velit cupiditate, doloremque, quis nobis harum nam exercitationem rem earum dolore fugit labore illum illo ab nesciunt eaque atque molestias? Error iure reprehenderit autem assumenda perspiciatis ullam!</p>
+	</div>
+}
+
+export default AboutPage
---
+++ src/app/about/team/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const TeamPage = () => {
+	return <div>
+		<h1>About Team</h1>
+		<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate vel alias, amet tempora unde quia debitis, iusto perspiciatis earum rem maiores beatae reprehenderit, minima sint! Voluptates nesciunt, illum vitae provident, officia sunt laboriosam molestiae reiciendis minus praesentium iusto? Perspiciatis molestias corporis fugit temporibus ad voluptates vitae debitis odit minus assumenda.</p>
+	</div>
+}
+
+export default TeamPage
